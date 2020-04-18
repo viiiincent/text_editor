51e7a575222561f0366bd567cfb28d168b35544f
Vincent Marger <vincent.marger@gmail.com>
2020-04-18 12:17:24 -0400

    Improve cursor navigation.

--- main.c
+++ main.c
@@ -360,21 +360,43 @@ void refresh_screen()
 
 void move_cursor(int key)
 {
+	erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch (key)
 	{
 		case ARROW_UP:
 			if (E.cy > 0) E.cy--;
 			break;
 		case ARROW_LEFT:
-			if (E.cx > 0) E.cx--;
+			if (E.cx != 0)
+			{
+				E.cx--;
+			}
+			else if (E.cy > 0) // first column and not first line? go to previous line end
+			{
+				E.cy--;
+				E.cx = E.row[E.cy].size;
+			}
 			break;
 		case ARROW_DOWN:
 			if (E.cy < E.numrows) E.cy++;
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if (row && E.cx < row->size)
+			{
+				E.cx++;
+			}
+			else if (row && E.cx == row->size) // end of line? go to next column start
+			{
+				E.cx = 0;
+				E.cy++;
+			}
 			break;
 	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if (E.cx > rowlen) E.cx = rowlen;
 }
 
 void process_key_press()
